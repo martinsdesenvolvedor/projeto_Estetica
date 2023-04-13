bfa7966a9f1ab990f54ea91da056e131e431aca0
martinsdesenvolvedor <107735914+martinsdesenvolvedor@users.noreply.github.com>
2023-04-13 02:21:13 -0300

    alteração cabeçalho, carrossel na sessão sobre mim

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-$('.galeria').slick();
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let i = 0;
+function carrossel() {
+    i++;
+    if(i > img.length -1) {
+        i = 0;
+    }
+    imgs.style.transform = `translateX(${-i * 500}px)`;
+}
+
+setInterval(carrossel, 2000);
